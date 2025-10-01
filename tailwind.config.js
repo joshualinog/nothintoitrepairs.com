4bec10e6a348823f3f80dc3f328fd5b68d52dbf1
Joshua Linog <jsinkcreations@gmail.com>
2025-10-01 14:55:22 -0400

    add styles and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,18 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Rubik", "ui-sans-serif", "system-ui", "sans-serif"],
+        sans: ["ui-sans-serif", "system-ui", "sans-serif"],
+        lexend: ["Lexend", "sans-serif"],
+        lexendTera: ["Lexend Tera", "sans-serif"],
+        mono: ["Roboto Mono", "monospace"],
+      },
+      colors: {
+        taupe: "#4a4235",
+        reseda: "#776e45",
+        black: "#020100",
+        khaki: "#b3ac9c",
+        coyote: "#776654",
+        aliceblue: "#f2f8fc",
       },
     },
   },
